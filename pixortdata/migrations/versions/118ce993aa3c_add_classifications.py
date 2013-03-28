c4a4976ccecf6358b69a4ac0c7e3d5b138cd4950
Mate Lakat <mlakat@mlakat.hu>
2013-03-28 07:52:54 +0000

    Added migration and fix script bugs around schema creation

---
+++ pixortdata/migrations/versions/118ce993aa3c_add_classifications.py
@@ -0,0 +1,28 @@
+"""Add Classifications
+
+Revision ID: 118ce993aa3c
+Revises: None
+Create Date: 2013-03-28 06:43:30.082680
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '118ce993aa3c'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table('classifications',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('category', sa.String(), nullable=True),
+        sa.Column('key', sa.String(), nullable=True),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('key')
+    )
+
+
+def downgrade():
+    op.drop_table('classifications')
--- pixortdata/scripts.py
+++ pixortdata/scripts.py
@@ -17,10 +17,11 @@ def init():
 
 
 def version():
-    parser = argparse.ArgumentParser(description='Initialise a database')
+    parser = argparse.ArgumentParser(
+        description='Print out the actual version of the database')
     parser.add_argument('dburl', help='Database URL for SQLAlchemy')
     args = parser.parse_args()
-    repositories.filesystem_alchemy_session(url=args.dburl, create_schema=True)
+    repositories.filesystem_alchemy_session(url=args.dburl, create_schema=False)
 
     from alembic.config import Config
     from alembic import command
@@ -31,10 +32,10 @@ def version():
 
 
 def upgrade():
-    parser = argparse.ArgumentParser(description='Initialise a database')
+    parser = argparse.ArgumentParser(description='Upgrade database schema')
     parser.add_argument('dburl', help='Database URL for SQLAlchemy')
     args = parser.parse_args()
-    repositories.filesystem_alchemy_session(url=args.dburl, create_schema=True)
+    repositories.filesystem_alchemy_session(url=args.dburl, create_schema=False)
 
     from alembic.config import Config
     from alembic import command
@@ -45,13 +46,13 @@ def upgrade():
 
 
 def revision():
-    parser = argparse.ArgumentParser(description='Create a new revision')
+    parser = argparse.ArgumentParser(description='Create a new db revision')
     parser.add_argument('dburl', help='Database URL for SQLAlchemy')
     parser.add_argument('message', help='Message for the new version')
     parser.add_argument('--autogenerate', help='Autogenerate',
                         action='store_true')
     args = parser.parse_args()
-    repositories.filesystem_alchemy_session(url=args.dburl, create_schema=True)
+    repositories.filesystem_alchemy_session(url=args.dburl, create_schema=False)
 
     from alembic.config import Config
     from alembic import command
@@ -59,6 +60,3 @@ def revision():
     cfg.set_main_option("script_location", "pixortdata:migrations")
     cfg.set_main_option("sqlalchemy.url", args.dburl)
     command.revision(cfg, message=args.message, autogenerate=args.autogenerate)
-
-
-
