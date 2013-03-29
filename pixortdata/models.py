3a7814cd81bdf901497fa1c24e0e298009c9bd94
Mate Lakat <mlakat@mlakat.hu>
2013-03-29 13:01:50 +0000

    Add tags to sqlalchemy

--- pixortdata/models.py
+++ pixortdata/models.py
@@ -6,7 +6,7 @@ class RawValue(object):
 
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
 
@@ -17,3 +17,26 @@ class SARaw(Base):
     id = Column(Integer, primary_key=True)
     raw_value = Column(String)
     key = Column(String, unique=True)
+
+
+class SATag(Base):
+    __tablename__ = 'tags'
+
+    id = Column(Integer, primary_key=True)
+    raw_id = Column(Integer, ForeignKey('raw.id'))
+    tag_id = Column(Integer, ForeignKey('tags.id'))
+
+
+class SACategory(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    classification_id = Column(Integer, ForeignKey('classifications.id'))
+    name = Column(String, nullable=False)
+
+
+class SAClassification(Base):
+    __tablename__ = 'classifications'
+
+    id = Column(Integer, primary_key=True)
+    category = Column(String)
